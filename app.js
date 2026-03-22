ea259667a2a4c7d51182df3b8051bbdba3f8f36f
Saamir Afraaz <saamirafraaz@gmail.com>
2026-03-22 05:47:14 +0530

    Basic Server and DB Setup Complete

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv').config()
+
+const DBconnect = (async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log('Connected to DB')
+    } catch(error) {
+        console.error('Cannot connect to DB')
+    }
+})
+
+DBconnect();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use((err,req,res,next) => {
+    res.status(500).json({message: err.message});
+})
+
+app.listen(3000,() => {
+    console.log("Connected to server");
+})
